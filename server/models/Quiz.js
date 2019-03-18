a4cc6da38c25e2f440ed278adf1aba8a070857c7
vat <vuanhtai1997@gmail.com>
2019-03-18 22:48:26 +0700

    [VAT]Fix model quiz

--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -1,29 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-require('dotenv').config();
 
-//Create Schema
 const QuizSchema = new Schema({
-  question: {
-    type: String,
-    required: true
-  },
-  answer: {
-    type: Array,
-    required: true
-  },
-  course_id: {
-    type: String,
-    required: true
-  },
   title: {
     type: String,
     required: true
   },
-  deadline: {
+  courseId: {
+    type: String,
+    required: true
+  },
+  listQuiz: [
+    {      
+      question: {
+        type: String
+      },
+      answers: [],
+      correctAnswer: [],
+      time: Date,
+      questionType: {
+        type: String
+      }
+    }
+  ],
+  deadline:{
     type: Date,
     required: true
   },
-});
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 module.exports = Quiz = mongoose.model('quiz', QuizSchema)
+
